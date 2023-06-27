d11e89d8b189f4ffded4330dc1a6bc64436c0424
손연수 <dark602@marketboro.com>
2023-06-27 18:02:38 +0900

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { useEffect } from 'react';
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function TestButtons (){
+  const [count, setCount] = useState(0);
+  return (
+    count < 2? 
+        <button onClick={() => setCount((count) => count + 1)}>
+      count is {count}
+    </button> : 
+    <div>2가 넘었어요;</div>
+  )
+}
 
+function App() {
+  useEffect(() => {
+   alert("여기 왔어요?")
+  });
+  
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <TestButtons></TestButtons>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
