3b9557c6639587a6a57d45e4fd0c54fce5aa348c
손연수 <dark602@marketboro.com>
2023-07-06 17:56:47 +0900

    recoil

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,6 +9,24 @@ import {
   useRecoilValue,
 } from 'recoil';
 
+const textState = atom({
+  key: 'textState', // unique ID (with respect to other atoms/selectors)
+  default: '', // default value (aka initial value)
+});
+
+const charCountState = selector({
+  key: 'charCountState', // unique ID (with respect to other atoms/selectors)
+  get: ({get}) => {
+    const text = get(textState);
+
+    return text.length;
+  },
+});
+function CharacterCount() {
+  const count = useRecoilValue(charCountState);
+  return <>Character Count: {count}</>;
+}
+
 function TestButtons (){
   const [count, setCount] = useState(0);
   return (
@@ -17,7 +35,30 @@ function TestButtons (){
     </button>
   )
 }
+function CharacterCounter() {
+  return (
+    <div>
+      <TextInput />
+      <CharacterCount />
+    </div>
+  );
+}
 
+function TextInput() {
+  const [text, setText] = useRecoilState(textState);
+
+  const onChange = (event) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <div>
+      <input type="text" value={text} onChange={onChange} />
+      <br />
+      Echo: {text}
+    </div>
+  );
+}
 function App() {
   return (
     <>
