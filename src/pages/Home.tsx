c0254c9dde8b9f5d1b7e143961f84fe74907c3f1
손연수 <dark602@marketboro.com>
2023-09-07 15:26:30 +0900

    test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,13 +2,17 @@ import React from "react"
 const height = {
   height: '100dvh'
 }
+const homeStyle = {
+  padding:'20px'
+}
 function Home() {
   return (
     <>
       <section style={height}>
-        <div>
-          블로그의 홈입니다
-          각종 컨텐츠를 제공할 예정입니다.
+        <div style={homeStyle}>
+          블로그의 홈입니다 <br />
+          홈에는 이것저것이 등장할 예정입니다.<br />
+          각종 컨텐츠를 제공할 예정입니다.<br />
         </div>
         <div>
           <button>태그태그</button>
