53b62a13def28b360edee5b659f2a51c8ad13d10
ginzason <dark602@hanmail.net>
2023-11-30 00:41:02 +0900

    game make ing

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,15 +1,12 @@
 import React, { Component } from 'react'
 import '../assets/css/square.scss'
-interface SquareProps {
-  value: string | null;
-  handleClick: () => void; // handleClick이라는 함수가 전달되어야 함
+
+const Square = ({onClick, value}) => {
+  return (
+    <button className='square' onClick={onClick}>
+    {value}
+  </button>
+  )
 }
-export default class Square extends Component<SquareProps> {
-  render() {
-    return (
-      <button className='square' onClick={this.props.handleClick}>
-        {this.props.value}
-      </button>
-    )
-  }
-}
+
+export default Square;
