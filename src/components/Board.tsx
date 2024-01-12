e4bf387083bc2f77d45a33552f475fc6c45c00bd
ginzason <dark602@hanmail.net>
2024-01-12 23:21:56 +0900

    승자 결정 업데이트

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -4,7 +4,32 @@ import Square from './Square'
 const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
-  const status = `Next Player ${xIsNext? 'X': 'O'}`
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6],      
+    ];
+    for (let i=0; i < lines.length;i++){
+      const [a,b,c] = lines[i];
+      if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+        return squares[a];
+      }
+    }
+    return null;
+  }
+  const winner = calculateWinner(squares); //승자확인
+  let status 
+  if(winner){
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext? "X" : "O");
+  }
   const handleClick = (i) => {
     const newSquares = squares.slice();
     newSquares[i] = xIsNext? 'X': 'O';
@@ -35,8 +60,7 @@ const Board = () => {
         </div>
       </div>
     </>
-  )
-  
+  )  
 }
 
 export default Board;
