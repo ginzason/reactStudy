2771bdbff70ef730473a9ddda57f0ba7634fa2cb
ginzason <dark602@hanmail.net>
2024-01-12 23:53:54 +0900

    컴포넌트 정리

--- src/pages/Tag.tsx
+++ src/pages/Tag.tsx
@@ -1,6 +1,47 @@
-import React from "react"
+import React, {useState}  from "react"
 import Board from "../components/Board"
 function Tag() {
+  const [history, setHistory] = useState([{
+    squares: Array(9).fill(null)
+  }])
+  
+  const [xIsNext, setXIsNext] = useState(true);
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6],      
+    ];
+    for (let i=0; i < lines.length;i++){
+      const [a,b,c] = lines[i];
+      if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+        return squares[a];
+      }
+    }
+    return null;
+  }
+  const current = history[history.length -1]
+  const winner = calculateWinner(current.squares); //승자확인
+  let status 
+  if(winner){
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext? "X" : "O");
+  }
+  const handleClick = (i) => {
+    const newSquares = current.squares.slice();
+    if(calculateWinner(newSquares) || newSquares[i]){
+      return ;
+    }
+    newSquares[i] = xIsNext? 'X': 'O';
+    setHistory([...history, {squares: newSquares}]);
+    setXIsNext(prev => !prev);
+  }
   return (
     <>
       <div>
@@ -8,7 +49,7 @@ function Tag() {
       </div>
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board squares={current.squares} status={status} onClick={(i) => handleClick(i)}/>
         </div>
         <div className="game-info">
 
